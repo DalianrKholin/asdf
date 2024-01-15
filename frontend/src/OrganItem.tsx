22147bccd346c14a370b2e598851e475367f006a
Tomasz Wysocki <0xdaydream@proton.me>
2024-01-15 22:43:15 +0100

    feat: add functionality to delete data

--- frontend/src/OrganItem.tsx
+++ frontend/src/OrganItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Organ } from './App'
 import './OrganItem.css'
 
@@ -7,17 +8,68 @@ interface Props {
 }
 
 const OrganItem = ({ organ, idx }: Props) => {
+    const [isEditing, setIsEditing] = useState(false)
+    const [organsData, setOrgansData] = useState(organ)
+    console.log(organsData)
+
+    const handleDelete = async () => {
+        const res = await fetch(
+            `http://localhost:8080/admin/itemList?id=${organ.Id}`,
+            {
+                method: 'DELETE',
+            }
+        )
+
+        console.log(res)
+
+        if (!res.ok) {
+            alert('error')
+        } else {
+            alert('deleted!')
+        }
+    }
+
+    const handleUpdate = () => {
+        alert('update')
+    }
+
+    const handleChange = (key: string, value: string) => {
+        setOrgansData(prevData => ({
+            ...prevData,
+            [key]: value,
+        }))
+    }
+
     return (
         <div key={idx} className='organ'>
             {Object.keys(organ).map(key => {
                 if (key === 'Id') {
                     return
                 }
-                return <div>{organ[key]}</div>
+                return isEditing ? (
+                    <input
+                        type='text'
+                        value={organsData[key]}
+                        onChange={e => handleChange(key, e.target.value)}
+                    />
+                ) : (
+                    <div>{organ[key]}</div>
+                )
             })}
             <div className='buttons'>
-                <button>Edit</button>
-                <button>Delete</button>
+                {!isEditing ? (
+                    <>
+                        <button onClick={() => setIsEditing(true)}>Edit</button>
+                        <button onClick={handleDelete}>Delete</button>
+                    </>
+                ) : (
+                    <>
+                        <button onClick={() => setIsEditing(false)}>
+                            Cancel
+                        </button>
+                        <button onClick={handleUpdate}>Update</button>
+                    </>
+                )}
             </div>
         </div>
     )
