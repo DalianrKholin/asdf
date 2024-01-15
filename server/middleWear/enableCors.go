c8cb3af4ab5d6ba3177981c771655beafc9a1635
DalianrKholin <osadowskikacper@gmail.com>
2024-01-15 23:37:56 +0100

    essa

--- server/middleWear/enableCors.go
+++ server/middleWear/enableCors.go
@@ -2,11 +2,11 @@ package middleWear
 
 import "net/http"
 
-func EnableCors(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func (s *ApiDbMiddleWear) EnableCors(next http.HandlerFunc)http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Access-Control-Allow-Origin", "*") // lub "*" dla wszystkich originów
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
         next.ServeHTTP(w, r)
-    })
+    }
 }
