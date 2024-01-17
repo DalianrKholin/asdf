6bbe552eb9ff750528a4b0999ccc171685c534e5
DalianrKholin <osadowskikacper@gmail.com>
2024-01-17 18:13:45 +0100

    essunia

--- backend/model/dataBaseModel/Product.go
+++ backend/model/dataBaseModel/Product.go
@@ -4,8 +4,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Product struct {
 	Id         primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	Name       string             `bson:"name" json:"name,omitempty"`
-	Price      float64            `bson:"price" json:"price,omitempty"`
-	Properties string             `bson:"properties" json:"properties,omitempty"`
-	InStack    int                `bson:"inStack" json:"inStack,omitempty"`
+	Name       string             `bson:"name" json:"name"`
+	Price      float64            `bson:"price" json:"price"`
+	Properties string             `bson:"properties" json:"properties"`
+	InStack    int                `bson:"inStack" json:"inStack"`
 }
